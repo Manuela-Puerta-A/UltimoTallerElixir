b401cbb7056c97ef2bbb4dfc25c60ebb006eee32
Manuela Puerta <Manuela.puertaa@uqvirtual.edu.co>
2025-11-13 22:19:27 -0500

    comits

--- taller/ejercicio4/test.exs
+++ taller/ejercicio4/test.exs
@@ -1,14 +1,14 @@
-reseñas = limpiezaResenas.generar_reseñas(100)
+resenas = LimpiezaResenas.generar_resenas(100)
 
-IO.puts("\nSecuencial")
-{resultado_sec, tiempo_sec} = Benchmark.medir(fn -> limpezaresenas.procesar_secuencial(reseñas) end)
-IO.puts("Reseñas procesadas: #{length(resultado_sec)}")
+IO.puts("\nsecuencial=")
+{resultado_sec, tiempo_sec} = Benchmark.medir(fn -> LimpiezaResenas.procesar_secuencial(resenas) end)
+IO.puts("Resenas procesadas: #{length(resultado_sec)}")
 IO.puts("Tiempo: #{tiempo_sec} ms")
 IO.puts("Primeras 3: #{inspect(Enum.take(resultado_sec, 3))}")
 
-IO.puts("\n Concurrente")
-{resultado_conc, tiempo_conc} = Benchmark.medir(fn -> limpiezaResenas.procesar_concurrente(reseñas) end)
-IO.puts("Reseñas procesadas: #{length(resultado_conc)}")
+IO.puts("\nconcurrente")
+{resultado_conc, tiempo_conc} = Benchmark.medir(fn -> LimpiezaResenas.procesar_concurrente(resenas) end)
+IO.puts("Resenas procesadas: #{length(resultado_conc)}")
 IO.puts("Tiempo: #{tiempo_conc} ms")
 IO.puts("Primeras 3: #{inspect(Enum.take(resultado_conc, 3))}")
 
